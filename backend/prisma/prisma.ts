444b4e476250985ad69ba6d06b18b897f55244f4
alexcsych <alexsejsi4@gmail.com>
2026-05-17 19:30:23 +0300

    feat(prisma): initialize PrismaClient with native pg adapter and singleton pattern

--- backend/prisma/prisma.ts
+++ backend/prisma/prisma.ts
@@ -1,5 +1,24 @@
-import { PrismaClient } from "@prisma/generated/client";
+import "dotenv/config";
+import { PrismaClient } from "./generated/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-const prisma = new PrismaClient();
+const pool = new Pool({
+  user: process.env["POSTGRES_USER"],
+  password: process.env["POSTGRES_PASSWORD"],
+  host: process.env["POSTGRES_HOST"],
+  port: Number(process.env["POSTGRES_PORT"]) ?? 5432,
+  database: process.env["POSTGRES_DB"],
+});
+const adapter = new PrismaPg(pool);
+
+const prismaClientSingleton = () => {
+  return new PrismaClient({ adapter });
+};
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const prisma = globalForPrisma.prisma || prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
